879f7026f5ae4355a2dc0c255f3305d038fb44f6
JawadKhan65 <jawadkhan10322@gmail.com>
2024-02-13 22:52:48 +0500

    iop

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ import cartRouter from "./routes/cartRoutes.js";
 import stripeRouter from "./routes/stripe.js";
 
 const app = express()
-app.use(cors())
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Check if the origin is allowed
+        const allowedOrigins = ['https://ecommerce-electron.vercel.app', 'https://another-allowed-origin.com'];
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json())
 app.use("/", (req, res) => {
     res.send("<h1>Hello World</h1>")
