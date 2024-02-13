f4684802fa850517e62dae746fff36cabdfa5e1c
JawadKhan65 <jawadkhan10322@gmail.com>
2024-02-13 21:30:32 +0500

    iop

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import cartRouter from "./routes/cartRoutes.js";
 import stripeRouter from "./routes/stripe.js";
 
 const app = express()
-app.use(cors())
+
 app.use(express.json())
 app.use("/", (req, res) => {
     res.send("<h1>Hello World</h1>")
