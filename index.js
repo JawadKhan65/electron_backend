bb765eb7135268707c1f4df0b1639f856df8aa66
JawadKhan65 <jawadkhan10322@gmail.com>
2024-02-13 21:50:32 +0500

    hell its done

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import cartRouter from "./routes/cartRoutes.js";
 import stripeRouter from "./routes/stripe.js";
 
 const app = express()
-
+app.use(cors({
+    origin: ["https://ecommerce-electron.vercel.app"],
+    methods: ["POST", "PUT", "DELETE", "GET"],
+    credentials: true
+}))
 app.use(express.json())
 app.use("/", (req, res) => {
     res.send("<h1>Hello World</h1>")
