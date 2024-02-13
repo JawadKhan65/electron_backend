f0b12bb139d3b00ecd68c2731e4716379f7d1a14
JawadKhan65 <jawadkhan10322@gmail.com>
2024-02-13 22:39:39 +0500

    cors changed

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ import cartRouter from "./routes/cartRoutes.js";
 import stripeRouter from "./routes/stripe.js";
 
 const app = express()
-app.use(cors({
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use("/", (req, res) => {
     res.send("<h1>Hello World</h1>")
