74337e91353cfcedd3fd9f1993b71510020753d7
JawadKhan65 <jawadkhan10322@gmail.com>
2024-02-15 00:46:03 +0500

    iloveu

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ import stripeRouter from "./routes/stripe.js";
 const app = express()
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
-    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-})
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json())
 
