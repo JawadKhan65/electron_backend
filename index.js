ab56dc9cb1749d234fac485d28249bdb1bdc0b60
JawadKhan65 <jawadkhan10322@gmail.com>
2024-02-16 00:27:35 +0500

    16

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ import stripeRouter from "./routes/stripe.js";
 const app = express()
 
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 app.use(express.json())
 
